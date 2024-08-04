4786461579162029e40c8543fe7f6372748a190d
Anh Nguyen <anh.nt@josys.com>
2024-08-04 15:39:50 +0700

    feat: middleware, routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+  "strings"
+  "unicode/utf8"
 
 	"snippetbox.anhnt2001/internal/models"
+  "github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -27,7 +26,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -49,21 +50,66 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+  data := app.newTemplateData(r)
 
-	title := "0 nail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+  app.render(w, http.StatusOK, "create.html", data)
+}
 
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    r.Body = http.MaxBytesReader(w, r.Body, 4096)
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    title := r.PostForm.Get("title")
+    content := r.PostForm.Get("content")
+
+    
+    expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    // Initialize a map to hold any validation errors for the form fields.
+    fieldErrors := make(map[string]string)
+
+    // Check that the title value is not blank and is not more than 100
+    // characters long. If it fails either of those checks, add a message to the
+    // errors map using the field name as the key.
+    if strings.TrimSpace(title) == "" {
+        fieldErrors["title"] = "This field cannot be blank"
+    } else if utf8.RuneCountInString(title) > 100 {
+        fieldErrors["title"] = "This field cannot be more than 100 characters long"
+    }
+
+    // Check that the Content value isn't blank.
+    if strings.TrimSpace(content) == "" {
+        fieldErrors["content"] = "This field cannot be blank"
+    }
+
+    // Check the expires value matches one of the permitted values (1, 7 or
+    // 365).
+    if expires != 1 && expires != 7 && expires != 365 {
+        fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+    }
+
+    // If there are any errors, dump them in a plain text HTTP response and
+    // return from the handler.
+    if len(fieldErrors) > 0 {
+        fmt.Fprint(w, fieldErrors)
+        return
+    }
+
+
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+      app.serverError(w, err)
+      return
+    }
+
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,27 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+ "github.com/julienschmidt/httprouter"
+  "github.com/justinas/alice"
+)
 
-func (app *application) routes() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) routes() http.Handler {
+  router := httprouter.New()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+  router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	return mux
+  // And then create the routes using the appropriate methods, patterns and 
+  // handlers.
+  router.HandlerFunc(http.MethodGet, "/", app.home)
+  router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+  router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+  router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+
+  // Create the middleware chain as normal.
+  standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+  // Wrap the router with the middleware and return it as normal.
+  return standard.Then(router)
 }
